b9f870c519acfa2cdf96c08d0346a14f39bd2ecc
Nikolaos Perrakis <nikperrakis@gmail.com>
2020-10-06 00:51:45 +0300

    only modify folder contents if user is owner

--- fcust/fcust.py
+++ fcust/fcust.py
@@ -4,6 +4,8 @@ Main module.
 
 from pathlib import PosixPath, Path
 from shutil import chown
+from pwd import getpwuid
+from os import getuid
 
 
 class CommonFolder:
@@ -37,6 +39,8 @@ class CommonFolder:
         else:
             self.group = common_group
 
+        self.user: str = getpwuid(getuid()).pw_name
+
     def enforce_permissions(self):
         """
         We read the contents of a specified directory and enforce unix permissions.
@@ -44,6 +48,10 @@ class CommonFolder:
         Files should have 664 permissions
         Folders should have 2775 permisions (ie also setguid bit)
         Group should be common golder's group.
+
+        The function only changes permissions if the user is owner of the relevant resource.
+        This is done to avoid the need for root access, but requires the function to be
+        run by all the users sharing the common folder.
         """
 
         # Let's iterate over folders we find first - glob returns a generator!
@@ -52,17 +60,17 @@ class CommonFolder:
             pil = Path(il)
 
             # Fix group membership if needed
-            if pil.group() != self.group:
+            if pil.group() != self.group and pil.owner() == self.user:
                 chown(pil, group=self.group)
 
             # Check folder permissions and fix if needed
             if pil.is_dir():
                 perms: str = oct(pil.stat().st_mode)[-4:]
-                if perms != "2775":
+                if perms != "2775" and pil.owner() == self.user:
                     pil.chmod(0o42775)
 
             # Check folder permissions and fix if needed
             if pil.is_file():
                 perms: str = oct(pil.stat().st_mode)[-4:]
-                if perms != "0664":
+                if perms != "0664" and pil.owner() == self.user:
                     pil.chmod(0o40664)
