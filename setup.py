51a17d5770826c541c9c48ebd9aef1b163d286bf
Nikolaos Perrakis <nikperrakis@gmail.com>
2021-11-21 13:29:05 +0200

    update pypi classifiers

--- setup.py
+++ setup.py
@@ -50,12 +50,11 @@ setup(
     author_email="nikperrakis@gmail.com",
     python_requires=">=3.8",
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: End Users/Desktop",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
     ],
